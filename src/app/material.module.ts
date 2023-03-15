756833ce4be759589eb2eae0e392cacddd1f9c1d
Gabriel Borrás Serra <biel40@Air-de-Gabriel.home>
2023-03-15 20:47:51 +0100

    style fixes and added component card for attack list

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   exports: [
     MatSelectModule,
     MatInputModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatCardModule
   ]
 })
 
