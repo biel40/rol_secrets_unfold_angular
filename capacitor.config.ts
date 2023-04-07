ec2eeb0db3e45355424e61c6d639b44e47976348
Gabriel Borrás Serra <biel40@Air-de-Gabriel.home>
2023-04-07 11:50:33 +0200

    sucesfully added capacitor to project and build to Kotlin native app working

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'rol-secrets-unfold-angular',
+  webDir: 'dist/rol_secrets_unfold_angular',
+  bundledWebRuntime: false
+};
+
+export default config;
